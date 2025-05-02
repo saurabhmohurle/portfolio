c4afbe253101c84d2ad2291db16c0a0a629b5328
Saurabh Mohurle <147239028+saurabhmohurle@users.noreply.github.com>
2025-05-02 22:51:43 +0530

    js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-function nextSlide(carouselId) {
-  const slides = document.querySelector(`#${carouselId} .slides`);
-  slides.appendChild(slides.firstElementChild);
-}
-
-function prevSlide(carouselId) {
-  const slides = document.querySelector(`#${carouselId} .slides`);
-  slides.prepend(slides.lastElementChild);
+function slide(sliderId, direction) {
+  const slider = document.getElementById(sliderId);
+  const slides = slider.querySelector(".slides");
+  const slideWidth = slides.children[0].clientWidth + 16; // include margin
+  const currentTransform = getComputedStyle(slides).transform;
+  const matrix = new WebKitCSSMatrix(currentTransform);
+  const currentX = matrix.m41;
+  const newX = currentX - direction * slideWidth;
+  slides.style.transform = `translateX(${newX}px)`;
 }
