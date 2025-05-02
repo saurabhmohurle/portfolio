5636b81fe09ff426e2947efaf53b80c905125619
Saurabh Mohurle <147239028+saurabhmohurle@users.noreply.github.com>
2025-05-02 22:39:12 +0530

    js

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const images = document.querySelectorAll('.project-img');
-  const centerImg = document.querySelector('.project-img.center');
+function nextSlide(carouselId) {
+  const slides = document.querySelector(`#${carouselId} .slides`);
+  slides.appendChild(slides.firstElementChild);
+}
 
-  images.forEach(img => {
-      img.addEventListener('mouseover', () => {
-          img.style.transform = 'scale(1.2)';
-      });
-
-      img.addEventListener('mouseout', () => {
-          img.style.transform = 'scale(1)';
-      });
-  });
-
-  // Add sliding functionality and arrow buttons
-});
+function prevSlide(carouselId) {
+  const slides = document.querySelector(`#${carouselId} .slides`);
+  slides.prepend(slides.lastElementChild);
+}
